e3986eb1ff3e7e324fcebb881c5280148004104c
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-11-07 17:14:15 +0700

    Delete Lib_C_healthy/C_healthy directory

--- Lib_C_healthy/C_healthy/__init__.py
+++
@@ -1,4 +0,0 @@
-from .C_healthy import BMI, BMR, TDEE, HDSD, WHR, LBM, BFP, BBW, IBW, MA, FFMI, RFM, VFR, BSA, VO2MAX, HSI, MMI, BFM
-
-__version__ = "0.2.5"
-__all__ = ["BMI", "BMR", "TDEE", "HDSD", "WHR", "LBM", "BFP", "BBW", "IBW", "MA", "FFMI", "RFM", "VFR", "BSA", "VO2MAX", "HSI", "MMI", "BFM"]
